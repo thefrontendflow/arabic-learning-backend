b1bf30841f104ba4a54b52b9deef4c390421a2af
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2024-12-25 13:55:08 +0100

    Implement 2FA feature

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,16 @@
 import {
   changePassword,
   checkAuthentication,
+  disable2FA,
+  enable2FA,
   forgetPassword,
   loginUser,
   registerUser,
   resendVerificationEmail,
   resetPassword,
+  verify2FA,
   verifyEmail,
+  verifyLogin2FA,
 } from "../controllers/authController.js";
 
 import { authMiddleware } from "../middleware/authMiddleware.js";
@@ -22,5 +26,9 @@ router.post("/forget-password", forgetPassword);
 router.post("/reset-password/:token", resetPassword);
 router.post("/change-password", authMiddleware, changePassword);
 router.get("/check-auth", authMiddleware, checkAuthentication);
+router.post("/enable-2fa", authMiddleware, enable2FA);
+router.post("/verify-2fa", authMiddleware, verify2FA);
+router.post("/verify-2faLogin", verifyLogin2FA);
+router.post("/disable-2fa", authMiddleware, disable2FA);
 
 export default router;
